a200c54019cf53f8920e23327cb789f6615347e7
Никита Гуреенков <nikitagureenkov@MacBook-Air-Nikita.local>
2022-03-21 22:37:26 +0300

    Mobile layout in progress...

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const roundedText = document.getElementById("roundedText");
+  const roundedText = document.querySelector(".first_rounded_text_wrapper");
   let strForRound =
     "ENGLISH BY HELEN ONLINE ENGLISH BY HELEN ONLINE ENGLISH BY HELEN ";
 
   for (let i = 0; i < strForRound.length; i++) {
     const letter = strForRound[i];
-    console.log(letter);
-    let newSpan = document.createElement("span");
-    newSpan.innerHTML = letter;
-    roundedText.appendChild(newSpan);
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = letter;
+	newDiv.classList.add('rounded_text_item');
+    roundedText.appendChild(newDiv);
   }
 
-  const letter = document.querySelectorAll(".rounded_text > span");
+  const letter = document.querySelectorAll(".rounded_text_item");
   let currentDeg = 0;
   let deg = 360 / letter.length;
 
   // for create
-  setTransformRotateForLetter();
+  createRoundOfLetter();
 
-  // for first animation
-  setTimeout(() => {
-    setTransformRotateForLetter();
-  }, 0);
+  const roundWrapper = document.querySelector('.first_rounded_text_wrapper');
 
-  // for repeat animation
-  setInterval(() => {
-    setTransformRotateForLetter();
-  }, 35000);
 
   // helen face animate
-  document.querySelector(".HelenFace").style.filter = "none";
+//  document.querySelector(".HelenFace").style.filter = "none";
 
   // animate neon text
-  let firstImg = document.querySelector(".firstSectionTitle");
+//  let firstImg = document.querySelector(".firstSectionTitle");
 
-  setTimeout(() => {
-    OnOffNeon(firstImg, "./img/english-online-on.png", 500);
-    OnOffNeon(firstImg, "./img/english-online-off.png", 550);
-    OnOffNeon(firstImg, "./img/english-online-off.png", 700);
-    OnOffNeon(firstImg, "./img/english-online-off.png", 750);
-    OnOffNeon(firstImg, "./img/english-online-on.png", 1000);
+//  setTimeout(() => {
+//    OnOffNeon(firstImg, "./img/english-online-on.png", 500);
+//    OnOffNeon(firstImg, "./img/english-online-off.png", 550);
+//    OnOffNeon(firstImg, "./img/english-online-off.png", 700);
+//    OnOffNeon(firstImg, "./img/english-online-off.png", 750);
+//    OnOffNeon(firstImg, "./img/english-online-on.png", 1000);
 
-    OnOffNeon(firstImg, "./img/english-online-off.png", 3000);
-    OnOffNeon(firstImg, "./img/english-online-on.png", 3100);
-  }, 0);
+//    OnOffNeon(firstImg, "./img/english-online-off.png", 3000);
+//    OnOffNeon(firstImg, "./img/english-online-on.png", 3100);
+//  }, 0);
 
   // scroll
 
@@ -55,23 +48,23 @@ document.addEventListener("DOMContentLoaded", () => {
 	thirdSection = document.getElementById('third'),
 	fourthSection = document.getElementById('fourth');
 
-  understandBtn.addEventListener("click", () => {
-	secondSection.scrollIntoView({behavior: 'smooth'})
-  });
+//  understandBtn.addEventListener("click", () => {
+//	secondSection.scrollIntoView({behavior: 'smooth'})
+//  });
 
-  scrollToWhyMe.addEventListener("click", () => {
-	secondSection.scrollIntoView({behavior: 'smooth'})
-  });
+//  scrollToWhyMe.addEventListener("click", () => {
+//	secondSection.scrollIntoView({behavior: 'smooth'})
+//  });
 
-  scrollToPrice.addEventListener("click", () => {
-	thirdSection.scrollIntoView({behavior: 'smooth'})
-  });
+//  scrollToPrice.addEventListener("click", () => {
+//	thirdSection.scrollIntoView({behavior: 'smooth'})
+//  });
 
-  scrollToLetsGo.addEventListener("click", () => {
-	fourthSection.scrollIntoView({behavior: 'smooth'})
-  });
+//  scrollToLetsGo.addEventListener("click", () => {
+//	fourthSection.scrollIntoView({behavior: 'smooth'})
+//  });
 
-  function setTransformRotateForLetter() {
+  function createRoundOfLetter() {
     letter.forEach((el) => {
       el.style.transform = `rotate(${currentDeg}deg)`;
       currentDeg += deg;
